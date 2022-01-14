3d2f6058d48129894ec95d90afa8e9844786e9b3
Noshan <noshan@finosys.com>
2022-01-14 13:59:02 +0500

    NS

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -2,8 +2,8 @@ class constants {
     static getConstant() {
       const envVariables = {
         // url: 'http://20.195.8.140:1510',
-        // url:'http://23.98.66.95:1510',
-        url:'https://ai-apps.azurewebsites.net/api/v1/',
+        url:'http://192.168.18.57:125/api/v1',
+        // url:'https://ai-apps.azurewebsites.net/api/v1/',
       };
   
       return envVariables;
