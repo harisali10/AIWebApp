3682efea8c59c085be35e7108d516e7716c454ca
C9008315 <C9008315@arcelik.com>
2022-02-15 16:01:07 +0500

    logs

--- src/views/Logs.js
+++ src/views/Logs.js
@@ -12,60 +12,60 @@ import 'react-toastify/dist/ReactToastify.css';
 const Logs = () => {
     const constant = constants.getConstant();
     const [tableData, setTableData] = useState({ rows: [], columns: [], TableName: '' })
-    const [showLoader, setShowLoader] = useState(false)
+    // const [showLoader, setShowLoader] = useState(false)
     // const toast = React.useRef(null);
 
 
-    const notify = async () => {
+    // const notify = async () => {
 
-        // const resolveAfter3Sec = new Promise((resolve, rej) => fetchLogs());
-        // resolveAfter3Sec.catch((er) => {
-        //     console.log("err",er)
-        // })
-        // toast.promise(
-        //     resolveAfter3Sec,
-        //     {
-        //         pending: 'Promise is pending',
-        //         success: 'Promise resolved 👌',
-        //         error: 'Promise rejected 🤯'
-        //     }
-        // )
-        const resolveAfter3Sec = new Promise(async (resolve, reject) => {
-            setShowLoader(true)
-            try {
-                const logsRes = await axios.post(`${constant.url}GetPredictionResults`, {
-                    Header: {
-                        Type: 'Log',
+    //     // const resolveAfter3Sec = new Promise((resolve, rej) => fetchLogs());
+    //     // resolveAfter3Sec.catch((er) => {
+    //     //     console.log("err",er)
+    //     // })
+    //     // toast.promise(
+    //     //     resolveAfter3Sec,
+    //     //     {
+    //     //         pending: 'Promise is pending',
+    //     //         success: 'Promise resolved 👌',
+    //     //         error: 'Promise rejected 🤯'
+    //     //     }
+    //     // )
+    //     const resolveAfter3Sec = new Promise(async (resolve, reject) => {
+    //         setShowLoader(true)
+    //         try {
+    //             const logsRes = await axios.post(`${constant.url}GetPredictionResults`, {
+    //                 Header: {
+    //                     Type: 'Log',
 
-                        ShopURL: sessionStorage.getItem("shop")
-                    }
-                })
-                setTableData((prevState) =>
-                    ({ ...prevState, rows: logsRes.data.Message.rows, columns: logsRes.data.Message.columns, TableName: "Logs" }))
-                resolve(1)
-            }
-            catch (e) {
-                reject(0)
-                // toast.current.show({ severity: 'error', summary: 'Failed to get Logs', detail: "Logs Api Response Failed", life: 3000 });
-            }
+    //                     ShopURL: sessionStorage.getItem("shop")
+    //                 }
+    //             })
+    //             setTableData((prevState) =>
+    //                 ({ ...prevState, rows: logsRes.data.Message.rows, columns: logsRes.data.Message.columns, TableName: "Logs" }))
+    //             resolve(1)
+    //         }
+    //         catch (e) {
+    //             reject(0)
+    //             // toast.current.show({ severity: 'error', summary: 'Failed to get Logs', detail: "Logs Api Response Failed", life: 3000 });
+    //         }
 
 
-            setShowLoader(false)
+    //         setShowLoader(false)
 
-        });
-        console.log({ resolveAfter3Sec })
-        toast.promise(
-            resolveAfter3Sec,
-            {
-                pending: 'Promise is pending',
-                success: 'Promise resolved 👌',
-                error: 'Promise rejected 🤯'
-            }
-        )
-    };
+    //     });
+    //     console.log({ resolveAfter3Sec })
+    //     toast.promise(
+    //         resolveAfter3Sec,
+    //         {
+    //             pending: 'Promise is pending',
+    //             success: 'Promise resolved 👌',
+    //             error: 'Promise rejected 🤯'
+    //         }
+    //     )
+    // };
 
     useEffect(() => {
-        // fetchLogs();
+        fetchLogs();
     }, [])
 
 
@@ -84,7 +84,7 @@ const Logs = () => {
                 ({ ...prevState, rows: logsRes.data.Message.rows, columns: logsRes.data.Message.columns, TableName: "Logs" }))
         }
         catch (e) {
-            // toast.current.show({ severity: 'error', summary: 'Failed to get Logs', detail: "Logs Api Response Failed", life: 3000 });
+            toast.current.show({ severity: 'error', summary: 'Failed to get Logs', detail: "Logs Api Response Failed", life: 3000 });
         }
         setShowLoader(false)
 
@@ -97,11 +97,11 @@ const Logs = () => {
 
         </BackDropLoader>
         {/* <Toast ref={toast} /> */}
-        <button onClick={notify}>Notify!</button>
+        {/* <button onClick={notify}>Notify!</button> */}
         <ToastContainer />
 
         {
-            !showLoader &&
+            // !showLoader &&
             <Register
                 columns={tableData.columns}
                 rows={tableData.rows}
