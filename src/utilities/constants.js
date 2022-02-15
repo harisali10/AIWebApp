4eb40b720d004c414116462a9b86000adb7c3ab7
C9008315 <C9008315@arcelik.com>
2022-02-15 12:00:44 +0500

    chng

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -4,8 +4,8 @@ class constants {
       // url: 'http://20.195.8.140:1510',
       // url:'http://23.98.66.95:1510',
       // url: 'http://localhost:125/api/v1/'
-      // url:'https://ai-apps.azurewebsites.net/api/v1/',
-      url:'https://929a-103-244-179-84.ngrok.io/api/v1/'      
+      url:'https://ai-apps.azurewebsites.net/api/v1/',
+      // url:'https://929a-103-244-179-84.ngrok.io/api/v1/'      
     };
 
     return envVariables;
